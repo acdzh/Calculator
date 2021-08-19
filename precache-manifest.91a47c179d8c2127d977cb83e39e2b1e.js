9c93eea6badfa20a8dbf2154564be522a811625d
acdzh <acdzh@outlook.com>
2021-08-19 07:57:24 +0000

    deploy to Github pages

---
+++ precache-manifest.91a47c179d8c2127d977cb83e39e2b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb4f5ad45eb9b4d22b76a39db9b6809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972029c69c18cd668db0",
+    "url": "/static/css/main.6cd4b57f.chunk.css"
+  },
+  {
+    "revision": "d369525e4798f8e61a1b",
+    "url": "/static/js/2.fc05ff06.chunk.js"
+  },
+  {
+    "revision": "697edf3f8c65cb7493191eec4c6ff26a",
+    "url": "/static/js/2.fc05ff06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972029c69c18cd668db0",
+    "url": "/static/js/main.0124b056.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
